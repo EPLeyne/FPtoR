61b2c22331d91ea2e187b5979487f1b0bca3243e
EPLeyne <emmettleyne@gmail.com>
2019-03-18 08:15:38 +1100

    Added explaining notes

--- NPItest.R
+++ NPItest.R
@@ -1,16 +1,19 @@
+# Scripts for creating wide file from long from FP
+
 library(tidyverse)
 
+# Create the name set of the  columns to be dropped
 col_drop <- c("fpScoreSetID","fpNodeId","sampleNum","ScoreSetCreationDate","Trait", "Time", "Row", "Column")
 
-
-NPI_Establishment <- NPItest %>% separate("Time", c("Date","Time"), sep = " ") %>% 
-  filter(Trait == "Establishment") %>% 
-  select(-(col_drop)) %>%
-  spread(key = "Date", value = "Value") #%>% 
+# Script for transferring from long to wide, keeping only establishment trait (NPI data)
+NPI_Establishment <- NPItest %>% separate("Time", c("Date","Time"), sep = " ") %>%  #separate time metadata into date and time
+  filter(Trait == "Establishment") %>% # Filter all traits except 'Establishment'
+  select(-(col_drop)) %>%  # Drop all columns from the col_drop 
+  spread(key = "Date", value = "Value") #%>% #Spread the data from long to wide by the 'Date'
 
 View(NPI_Establishment)
 
-
+# Script for transferring from long to wide, keeping only heading trait (NPI data)
 NPI_Heading <- NPItest %>% separate("Time", c("Date","Time"), sep = " ") %>% 
   filter(Trait == "Heading_number") %>% 
   select(-(col_drop)) %>%
@@ -19,7 +22,7 @@ NPI_Heading <- NPItest %>% separate("Time", c("Date","Time"), sep = " ") %>%
 View(NPI_Heading)
 
 
-
+# Script for transferring from long to wide, keeping only anthesis trait (NPI data)
 NPI_Anthesis <- NPItest %>% separate("Time", c("Date","Time"), sep = " ") %>% 
   filter(Trait == "Anthesis_number") %>% 
   select(-(col_drop)) %>%
